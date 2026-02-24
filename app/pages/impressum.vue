1ddfa232dcd454cfd760d9507c799889489b202e
Linus Fischer <leicraft@leicraftmc.de>
2026-02-24 15:25:31 +0100

    update

--- app/pages/impressum.vue
+++ app/pages/impressum.vue
@@ -1,22 +1,52 @@
 <template>
     <div>
-        <h1 class="text-4xl font-bold mb-6">Impressum</h1>
-
-        <h3 class="text-2xl font-semibold mb-4">Angaben gemäß § 5 TMG</h3>
-
-        <strong class="font-bold">Betreiber:</strong>
-        <p class="text-base mb-6">
-            Linus Fischer<br>
-            Stötteritzer Str. 19<br>
-            04317 Leipzig<br>
-            Deutschland
+        <h1>Impressum</h1>
+        <p>Angaben gemäß § 5 DDG</p>
+        <p>
+            <strong>Linus Fischer</strong>
+            <br>
+            Stötteritzer Str. 19
+            <br>
+            04317 Leipzig
+            <br>
         </p>
+        <p>
+            <strong>Vertreten durch: </strong>
+            <br>
+            Linus Fischer
+            <br>
+        </p>
+        <p>
+            <strong>Kontakt:</strong>
 
-        <strong class="font-bold">Kontakt:</strong>
-        <p class="text-base mb-4">
-            E-Mail: <ULink to="mailto:support@leicraftmc.de" target="_blank">support@leicraftmc.de</ULink><br>
-            Tel: <ULink to="tel:+4934196258740" target="_blank">+49 341 9625 8740</ULink><br>
-            WhatsApp: <ULink to="https://wa.me/4934196258740" target="_blank" external>+49 341 9625 8740</ULink>
+            <br>
+            E-Mail: <a href="mailto:support@leicraftmc.de" target="_blank">support@leicraftmc.de</a>
+            <br>
+            Telefon: <a href="tel:+4934196258740" target="_blank">+49 341 9625 8740</a>
+            <br>
+            WhatsApp: <a href="https://wa.me/4934196258740" target="_blank">+49 341 9625 8740</a>
+            </br>
+        </p>
+        <p>
+            <strong>Verantwortlich für den Inhalt nach § 18 Abs. 2 MStV:</strong>
+
+            <br>
+            Linus Fischer
+            <br>
+            Stötteritzer Str. 19
+            <br>
+            04317 Leipzig
+            <br>
+        </p>
+        <p>
+            <strong>Verbraucherstreitbeilegung / Universalschlichtungsstelle</strong>
+            <br>
+            Wir nehmen nicht an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teil und sind dazu
+            auch nicht verpflichtet.
+        </p>
+        <p>
+            <strong>Datenschutzerklärung</strong><br>
+            Unter dem folgenden Link finden Sie unsere <a href="/datenschutz" target="_blank">Datenschutzerklärung</a>.
         </p>
     </div>
 </template>
